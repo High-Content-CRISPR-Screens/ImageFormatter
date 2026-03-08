b0e0f897d8eecf6754b46bb081b2054801786dd5
Andre Dias <aecd@chem.ku.dk>
2026-03-08 12:13:11 +0100

    Update main_scanr.py

--- main_scanr.py
+++ main_scanr.py
@@ -3,25 +3,19 @@ Author: André Dias, 2026"""
 
 import os
 import tifffile as tiff
-from HtImageFormatter import HtImageFormatter
+from HtImageFormater import HtImageFormater
 
 def main():
     # --- Initialization ---
-    image_formatter = HtImageFormatter()
+    image_formater = HtImageFormater()
 
     # --- User input for image directory ---
     img_dir = input("Enter the path to the image directory: ").strip('"')
+    output_dir = input("Enter the path to the output directory: ").strip('"')
 
     # --- Read and process images ---
-    image_formatter.read_ScanR(img_dir)
-    
-    output_dir = os.path.join(img_dir, "tiff_images")
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    
-    for i in range(image_formatter.nImages()):
-        img = image_formatter.prepare_multichannel_image(i)
-        tiff.imwrite(os.path.join(output_dir, f"image_{i}.tif"), img)
+    image_formater.read_ScanR(img_dir)
+    image_formater.export_images(output_dir=output_dir, channels=[2,3])
 
 if __name__ == "__main__":
     main()
